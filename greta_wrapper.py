798947153c12c72b4f876a3e688cca8b66f312fc
Adam <Drozynski@ifo.de>
2022-04-14 13:18:53 +0200

    changed some of it'

--- greta_wrapper.py
+++ greta_wrapper.py
@@ -13,7 +13,8 @@ def fetch_and_summarise_data_by_type_country(type, subtype, country):
         data_input_ts = data_input_ts.applymap(str_comma_to_float)
 
         data_input_stats = pd.read_csv(config.PATH_TO_STATS_FILE(type, subtype, country), sep=';')
-        data_input_stats = data_input_stats[['Power_Potential_GW', 'Power_Potential_Weighted_GW']]
+        data_input_stats = data_input_stats[['Power_Potential_Weighted_GW', 'FLH_Mean_Masked',
+                'FLH_Median_Masked', 'Energy_Potential_Weighted_TWh']]
         data_input_stats = data_input_stats.applymap(str_comma_to_float)
 
         return {'type': type, 'subtype': subtype, 'country': country,
@@ -21,8 +22,13 @@ def fetch_and_summarise_data_by_type_country(type, subtype, country):
             'FLH_q90': sum(data_input_ts['q90']), 'FLH_q70': sum(data_input_ts['q70']),
             'FLH_q50': sum(data_input_ts['q50']), 'FLH_q30': sum(data_input_ts['q30']),
             'FLH_q10': sum(data_input_ts['q10']),
-            'Power_Potential_GW': mean(data_input_stats['Power_Potential_GW']),
-            'Power_Potential_Weighted_GW': mean(data_input_stats['Power_Potential_Weighted_GW'])}
+            'Power_Potential_Weighted_GW': mean(data_input_stats['Power_Potential_Weighted_GW']),
+            'Energy_Potential_By_FLH_Mean_Weighted':
+                mean(data_input_stats['Energy_Potential_Weighted_TWh'])/
+                    mean(data_input_stats['FLH_Mean_Masked'])*1000,
+            'Energy_Potential_By_FLH_Median_Weighted':
+                mean(data_input_stats['Energy_Potential_Weighted_TWh'])/
+                    mean(data_input_stats['FLH_Median_Masked'])*1000}
     except FileNotFoundError:
         print(f'No data for: {type}, {subtype}, {country}, {config.YEAR}')
         return None
@@ -31,8 +37,9 @@ def fetch_and_summarise_data_by_type_country(type, subtype, country):
         print(data_input_ts.head())
 
 def fetch_and_summarise_data():
-    data = pd.DataFrame(columns=['type', 'subtype', 'country', 'FLH_q99', 'FLH_q95', 'FLH_q90', 'FLH_q70', 'FLH_q50',
-        'FLH_q30', 'FLH_q10', 'Power_Potential_GW', 'Power_Potential_Weighted_GW'])
+    data = pd.DataFrame(columns=['type', 'subtype', 'country', 'FLH_q99', 'FLH_q95', 'FLH_q90',
+        'FLH_q70', 'FLH_q50', 'FLH_q30', 'FLH_q10', 'Power_Potential_Weighted_GW',
+        'Energy_Potential_By_FLH_Mean_Weighted', 'Energy_Potential_By_FLH_Median_Weighted'])
 
     for type in config.TYPES:
         for subtype in config.SUBTYPES[type]:
